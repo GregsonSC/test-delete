505e300f92ddfe965fef1514e682a0fddb82848c
Kofi <107807110+Kof27@users.noreply.github.com>
2025-03-27 15:25:31 -0500

    [ADD]  NotFound component with 404 error page layout

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { Navbar } from "@/presentation/organisms/navbar/navbar";
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <header>
+                <Navbar />
+            </header>
+            <main className="flex-grow flex flex-col items-center justify-center">
+                <h1 className="text-9xl xl:text-[200px] font-black mb-6 text-[#ADE73A] ">404</h1>
+                <h1 className='font-bold text-5xl mb-4'>Oops!</h1>
+                <h2 className='text-4xl'>Page Not Found</h2>
+                <Link href="/">
+                </Link>
+            </main>
+        </div>
+    );
+}
