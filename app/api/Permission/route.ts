9940c43289c90a6213fed1b089d2c068fd771834
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2025-03-27 00:54:32 -0500

    [FL] [ADD] add get all permissions.

--- app/api/Permission/route.ts
+++ app/api/Permission/route.ts
@@ -1,14 +1,22 @@
-import {NextResponse} from "next/server"
-import db from '@/lib/prisma'
+import { NextResponse } from "next/server";
+import db from "@/lib/prisma";
 
-export async function POST(request){
-    const data = await request.json();
-    console.dir(data, { depth: null });
+export async function POST(request) {
+  const data = await request.json();
 
-
-    const newPermission = await db.permission.create({
-        data
-    });
-    return NextResponse.json(newPermission);
+  const newPermission = await db.permission.create({
+    data,
+  });
+  return NextResponse.json(newPermission);
 }
 
+export async function GET() {
+  try {
+    const permissions = await db.permission.findMany(); // Get all permissions
+    console.dir(permissions, { depth: null });
+    return NextResponse.json(permissions);
+  } catch (error) {
+    console.error("Error to get permissions: ", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
