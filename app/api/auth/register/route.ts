d3e47813df12cfa83afa926c22d27e59aec48a6d
Chologalactico <116129679+Chologalactico@users.noreply.github.com>
2025-03-29 00:21:31 -0500

    [UPDATE]User and roles

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,59 +1,94 @@
-import { NextResponse,NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import db from "@/lib/prisma";
 
 export async function POST(request: NextRequest) {
-
-  const data = await request.json();
-  
-  const userFound = await db.user.findUnique({
-    where: {
-      email: data.email,
-    },
-  });
-
-  if (userFound) {
-    return NextResponse.json(
-      {
-        message: "User with this email already exists",
-      },
-      {
-        status: 400,
-      }
-    );
-  }
-
-  const newUser = await db.user.create({
-    data,
-  });
-
-  return NextResponse.json(newUser);
-}
-export async function GET() {
   try {
-    
-    const users = await db.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        phone: true,
-        imageUrl: true,
+    const data = await request.json();
+
+    const userFound = await db.user.findUnique({
+      where: {
+        email: data.email,
       },
     });
-    return NextResponse.json(users);
+
+    if (userFound) {
+      return NextResponse.json({ message: "User with this email already exists" }, { status: 400 });
+    }
+
+    const newUser = await db.user.create({ data });
+
+    return NextResponse.json(newUser, { status: 201 });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json({ message: "Error creating role", error }, { status: 500 });
+  }
+}
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const requestId = searchParams.get("id");
+
+    if (!requestId) {
+      const users = await db.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+          imageUrl: true,
+        },
+      });
+      return NextResponse.json(users);
+    }
+    const id = Number(requestId);
+    if (isNaN(id)) {
+      return NextResponse.json({ error: "The id must be a valid number" }, { status: 400 });
+    }
+    const user = await db.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    return NextResponse.json(user);
   } catch (error) {
     console.error("Error fetching users:", error);
-    return NextResponse.json({ message: "Error fetching users" }, { status: 500 });
+    return NextResponse.json({ message: "Error fetching users", error }, { status: 500 });
   }
 }
 
-export async function PUT(request: NextRequest) {
+export async function PUT(request: Request) {
   try {
-    const { id, ...data } = await request.json();
-    
-    const userExists = await db.user.findUnique({ where: { id } });
-    if (!userExists) {
-      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    const { searchParams } = new URL(request.url);
+    const requestId = searchParams.get("id");
+    const data = await request.json();
+
+    if (!data) {
+      return NextResponse.json({ message: "No data provided" }, { status: 400 });
+    }
+
+    const id = Number(requestId);
+    if (isNaN(id) || !requestId) {
+      return NextResponse.json({ error: "The ID must be a valid number" }, { status: 400 });
+    }
+
+    const user = await db.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    // Verifica si el nuevo email ya está en uso por otro usuario
+    if (data.email) {
+      const existingUser = await db.user.findUnique({
+        where: { email: data.email },
+      });
+
+      if (existingUser && existingUser.id !== id) {
+        return NextResponse.json({ error: "Email already in use" }, { status: 400 });
+      }
     }
 
     const updatedUser = await db.user.update({
@@ -68,23 +103,27 @@ export async function PUT(request: NextRequest) {
   }
 }
 
-export async function DELETE(request: NextRequest) {
+export async function DELETE(request: Request) {
   try {
-    //Constante para buscar el id
-    const { id } = await request.json();
+    const { searchParams } = new URL(request.url);
+    const requestId = searchParams.get("id");
 
-    //Constante para ver si existe el valor en la base de datos
+    const id = Number(requestId);
+
     const userExists = await db.user.findUnique({ where: { id } });
-    //si el valor no existe que nos de un fallo que el usuario/id no existe
+
     if (!userExists) {
       return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
 
-    await db.user.delete({ where: { id } });
-    //En tal caso de que los usuarios si elimino nos lance un mensaje
+    const user = await db.user.delete({ where: { id } });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
     return NextResponse.json({ message: "User deleted successfully" }, { status: 200 });
   } catch (error) {
     console.error("Error deleting user:", error);
-    return NextResponse.json({ message: "Error deleting user",error}, { status: 500 });
+    return NextResponse.json({ message: "Error deleting user", error }, { status: 500 });
   }
 }
--- app/api/role/route.ts
+++ app/api/role/route.ts
@@ -17,31 +17,104 @@ export async function POST(request: NextRequest) {
     }
   } catch (error) {
     console.error("Error creating role:", error);
-    return NextResponse.json({ message: "Error creating role",error }, { status: 500 });
+    return NextResponse.json({ message: "Error creating role", error }, { status: 500 });
   }
 }
 //Role all
-export async function GET() {
+export async function GET(request: Request) {
   try {
-    const role = await db.role.findMany({
-      select: {
-        id: true,
-        name: true,
-        description: true,
-      },
+    const { searchParams } = new URL(request.url);
+    const requestId = searchParams.get("id");
+
+    if (!requestId) {
+      const roles = await db.role.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+        },
+      });
+      return NextResponse.json(roles);
+    }
+    const id = Number(requestId);
+    if (isNaN(id)) {
+      return NextResponse.json({ message: "The id must be a valid number" }, { status: 400 });
+    }
+    const role = await db.role.findUnique({
+      where: { id },
     });
-    if (!role || role.length === 0) {
-      return NextResponse.json({ message: "No roles found" }, { status: 404 });
+    if (!role) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
     return NextResponse.json(role, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       {
         message: "Error fetching role",
+        error,
       },
       {
         status: 500,
       }
     );
+  }
+}
+
+export async function PUT(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const requestId = searchParams.get("id");
+    const data = await request.json();
+
+    if (!data) {
+      return NextResponse.json({ message: "No data provided" }, { status: 400 });
+    }
+
+    const id = Number(requestId);
+    if (isNaN(id) || !requestId) {
+      return NextResponse.json({ error: "The ID must be a valid number" }, { status: 400 });
+    }
+
+    const role = await db.role.findUnique({
+      where: { id },
+    });
+
+    if (!role) {
+      return NextResponse.json({ error: "role not found" }, { status: 404 });
+    }
+    const updatedRole = await db.role.update({
+      where: { id },
+      data,
+    });
+
+    return NextResponse.json(updatedRole);
+  } catch (error) {
+    console.error("Error updating role:", error);
+    return NextResponse.json({ message: "Error updating role", error }, { status: 500 });
+  }
+}
+
+export async function DELETE(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const requestId = searchParams.get("id");
+
+    const id = Number(requestId);
+
+    const roleExists = await db.role.findUnique({ where: { id } });
+
+    if (!roleExists) {
+      return NextResponse.json({ message: "role not found" }, { status: 404 });
+    }
+
+    const role = await db.role.delete({ where: { id } });
+    if (!role) {
+      return NextResponse.json({ message: "role not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Role deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting role:", error);
+    return NextResponse.json({ message: "Error deleting role", error }, { status: 500 });
   }
 }
