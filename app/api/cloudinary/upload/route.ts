e7b2db0aeb353e66f2960a8c68e53ae2734f9ed4
FABIAN ESTEBAN LOPEZ ARIAS <fabian_esteban.lopez@uao.edu.co>
2025-04-28 09:03:21 -0500

    [FL] [MODIFY] Delete function.

--- app/api/cloudinary/upload/route.ts
+++ app/api/cloudinary/upload/route.ts
@@ -47,35 +47,3 @@ export async function createImage(formData: FormData) {
     // return NextResponse.json({ error: "Internal server error." }, { status: 500 });
   }
 }
-
-/*
-//"Handler for POST requests (image upload)."
-export async function POST(request: Request) {
-  try {
-    //Extracts the form data sent in the request.
-    const data = await request.formData();
-    const file = data.get("file");
-
-    //"Validates that a file has been provided and that it is a valid instance of File."
-    if (!file || !(file instanceof File)) {
-      return NextResponse.json("No valid image file provided.", {
-        status: 400,
-      });
-    }
-
-    // Converts the file into a byte buffer so it can be processed by Cloudinary.
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-
-    const result = await uploadImageToCloudinary(buffer);
-    // Returns a JSON response with the message and the secure URL of the uploaded image.
-    return NextResponse.json({
-      message: "Image uploaded.",
-      url: result.secure_url,
-    });
-  } catch (error) {
-    // Global error handling: logs the error to the console and responds with a 500 error.
-    console.error("Error uploading image to Cloudinary.:", error);
-    return NextResponse.json({ error: "Internal server error." }, { status: 500 });
-  }
-}*/
