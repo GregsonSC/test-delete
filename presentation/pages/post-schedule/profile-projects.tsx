1db33960880af82cbe26406378a182917a7d5409
Santiago2729 <107807110+Kof27@users.noreply.github.com>
2025-05-16 09:54:42 -0500

    [FEAT]Now post-schedule can only be accessed if there is a registered email in sessionstorage, otherwise it will be sent to notFound(). Also functionality was assigned to the buttons and now when a request is made an email is sent to the client confirming his appointment.

--- presentation/pages/post-schedule/profile-projects.tsx
+++ presentation/pages/post-schedule/profile-projects.tsx
@@ -8,7 +8,7 @@ import { BadgeCheck } from 'lucide-react';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import { ContactInfo } from "@/presentation/molecules/contact-info/contact-info";
-
+import { notFound } from "next/navigation";
 export function PostSchedule() {
   return (
     useEffect(() => {
@@ -22,15 +22,18 @@ export function PostSchedule() {
         day: "2-digit"
       });
 
+      if (!storedData.email){
+        notFound();
+      }
+
       DefaultToast.title(
         "Your request has been received!",
-        `Hello ${storedData.name}, your meeting is booked for ${formattedDate} between ${storedData.timeRange} in utc-5 timezone. We will contact you soon to confirm the details. Thank you for choosing us!`
+        `Hello ${storedData.name}, your meeting is booked for ${formattedDate} between ${storedData.timeRange} in utc-4 timezone. We will contact you soon to confirm the details. Thank you for choosing us!`
       );
       console.log("Información guardada en sessionStorage:", storedData);
       console.log("timeStart:", storedData.timeStart);
       console.log("timeFinish:", storedData.timeFinish);
     }, []),
-
     <MainLayout>
       <div className="lg:bg-[url('/fondos/RequestConfirmation.png')] lg:bg-cover lg:bg-no-repeat">
         {/* Main Content */}
@@ -71,8 +74,12 @@ export function PostSchedule() {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper vitae lacus eu convallis. Phasellus ut lectus dolor. Vestibulum eleifend orci non dapibus tincidunt.
               </CardContent>
               <CardFooter className="mt-10  place-content-center items-center flex gap-5 lg:pl-20">
+              <Link href="/register">
                 <Button className="rounded-full text-[#050A2B] font-bold md:text-2xl">Go to register</Button>
+              </Link>
+              <Link href="/login">
                 <Button className="rounded-full text-white font-bold md:text-2xl bg-transparent border-2 border-[#99CC33]">Log In</Button>
+              </Link>
               </CardFooter>
             </Card>
           </div>
